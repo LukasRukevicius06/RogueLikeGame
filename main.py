e0aac76f8c6acc5573a1ac24dea328100245200d
rukel001.318 <lrukevicius10@srrcc.org.uk>
2023-10-12 14:26:28 +0100

    Imported pygame, made the screen, added colours, imported an image and placed the image on the screen as an object

--- main.py
+++ main.py
@@ -1,3 +1,47 @@
-# A new file to work from
+import pygame
+import random
 
-print("Hello world")
+black = (0, 0, 0)
+white = (255, 255, 255)
+lightRed = ("#F00022")
+weakRed = ("#BD263A")
+greyRed = ("#8A3742")
+grey = ("#574544")
+
+
+pygame.init()
+
+display_width = int(1440)
+display_height = int(720)
+screenSize = (display_width, display_height)
+
+screen = pygame.display.set_mode(screenSize)
+pygame.display.set_caption('Platform Game')
+clock = pygame.time.Clock()
+screen.fill(lightRed)
+
+crashed = False
+
+ballImg = pygame.image.load("RedBall.png")
+
+
+def ball(x, y):
+    screen.blit(ballImg, (x, y))
+
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    screen.fill(lightRed)
+    ball(0, 0)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()

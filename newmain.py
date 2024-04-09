d3ff6e7c6be5622bd76a889acceac7689cc8f0bf
Lukas <lrukevicius10@srrcc.org.uk>
2024-04-10 00:10:24 +0100

    updates

--- newmain.py
+++ newmain.py
@@ -19,6 +19,8 @@ background = py.image.load('ground.png').convert_alpha()
 w = background.get_width()
 h = background.get_height()
 background = py.transform.scale(background, (w * 3, h * 3))
+crosshair = py.image.load('crosshair.png').convert_alpha()
+crosshair = py.transform.smoothscale_by(crosshair, 0.1)
 
 
 class Player(py.sprite.Sprite):
@@ -26,33 +28,57 @@ class Player(py.sprite.Sprite):
         super().__init__()
         self.image = py.transform.rotozoom(py.image.load("player_sprite.png").convert_alpha(), 0, 1)
         self.image = py.transform.scale(self.image, (100 * player_size, 130 * player_size))
+        self.base_image = self.image
         self.pos = py.math.Vector2(player_start_x, player_start_y)
         self.rect = self.image.get_rect(center=self.pos)
         self.center = self.rect.center
         self.speed = player_speed
+        self.mouse_coordinates = [0, 0]
+        self.vel_x = 0
+        self.vel_y = 0
 
     def main(self):
         self.inputs()
-        py.draw.rect(display, (0, 255, 0), self.rect)
+        self.mouse()
+        self.move()
+        self.player_rotation()
+
+    def move(self):
+        self.pos += (self.vel_x, self.vel_y)
+        py.draw.rect(display, (0, 255, 0), self.rect, width=2)
         display.blit(self.image, (self.pos[0], self.pos[1]))
 
     def inputs(self):
+        self.vel_x = 0
+        self.vel_y = 0
         self.speed = player_speed
 
         keys = py.key.get_pressed()
 
         if keys[py.K_a]:
             display_scroll[0] -= self.speed
+            self.vel_x = -self.speed
         if keys[py.K_d]:
             display_scroll[0] += self.speed
+            self.vel_x = self.speed
         if keys[py.K_w]:
             display_scroll[1] -= self.speed
+            self.vel_y = -self.speed
         if keys[py.K_s]:
             display_scroll[1] += self.speed
+            self.vel_y = self.speed
 
-        if display_scroll[0] != 0 and display_scroll[1] != 0:
-            self.speed /= math.sqrt(2)
-            self.speed /= math.sqrt(2)
+        self.mouse_coordinates = [py.mouse.get_pos()[0], py.mouse.get_pos()[1]]
+
+    def mouse(self):
+        display.blit(crosshair, (self.mouse_coordinates[0], self.mouse_coordinates[1]))
+
+    def player_rotation(self):
+        self.x_change_mouse_player = self.mouse_coordinates[0] - self.rect.centerx
+        self.y_change_mouse_player = self.mouse_coordinates[1] - self.rect.centery
+        self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
+        self.image = py.transform.rotate(self.base_image, -self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 
 player = Player()
@@ -60,10 +86,13 @@ player = Player()
 display_scroll = [0, 0]
 
 
+
 def draw_display():
-    display.blit(background, (-1000, -3000))
-    player.main()
+    display.fill((0, 0, 0))
+    display.blit(background, (-1000 - display_scroll[0], -3000 - display_scroll[1]))
     py.draw.rect(display, (255, 255, 255), (100 - display_scroll[0], 100 - display_scroll[1], 16, 16))
+    player.main()
+    # py.draw.rect(display, (255, 255, 255), (100 - display_scroll[0], 100 - display_scroll[1], 16, 16))
     py.display.update()
 
 
@@ -80,3 +109,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
